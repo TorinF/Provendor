faee3a01167e1e80031133d9261efd8207cd3380
Adhithya <ady.vijay@gmail.com>
2019-06-09 09:01:55 -0600

    check

--- app/src/main/java/com/Provendor/Provendor/Questions.java
+++ app/src/main/java/com/Provendor/Provendor/Questions.java
@@ -10,14 +10,31 @@ public class Questions extends AppCompatActivity {
     private TextView date;
     private TextView q;
     private TextView name;
-    private TextView Karma;
-    private TextView Answered;
+    private TextView karma;
+    private TextView answered;
     private QuestionsClass test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
-        
+        date = (TextView) findViewById(R.id.Date);
+        q = (TextView) findViewById(R.id.Question);
+        name = (TextView) findViewById(R.id.UserName);
+        karma = (TextView) findViewById(R.id.Karma);
+        answered = (TextView) findViewById(R.id.Answered);
+        test = new QuestionsClass();
+
+        date.setText(test.getDate());
+        q.setText(test.getDate());
+        name.setText(test.getUser());
+        karma.setText("Karma: " + test.getKarma());
+        if(test.isAnswered()){
+            answered.setText("This question has been answered");
+        }
+        else{
+            answered.setText("This question has not been answered");
+        }
+
     }
 }
