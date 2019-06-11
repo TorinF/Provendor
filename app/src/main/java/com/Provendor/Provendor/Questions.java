d7e497c6594868df38ea2ad9349872de4aa762a2
Adhithya <ady.vijay@gmail.com>
2019-06-11 10:01:58 -0600

    check

--- app/src/main/java/com/Provendor/Provendor/Questions.java
+++ app/src/main/java/com/Provendor/Provendor/Questions.java
@@ -3,6 +3,8 @@ package com.Provendor.Provendor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Questions extends AppCompatActivity {
@@ -12,6 +14,7 @@ public class Questions extends AppCompatActivity {
     private TextView name;
     private TextView karma;
     private TextView answered;
+    private Button karmaGive ;
     private QuestionsClass test;
 
     @Override
@@ -23,8 +26,14 @@ public class Questions extends AppCompatActivity {
         name = (TextView) findViewById(R.id.UserName);
         karma = (TextView) findViewById(R.id.Karma);
         answered = (TextView) findViewById(R.id.Answered);
+        karmaGive = (Button) findViewById(R.id.Karmagiver);
         test = new QuestionsClass();
-
+        karmaGive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                test.setKarma(test.getKarma()+1);
+            }
+        });
         date.setText(test.getDate());
         q.setText(test.getDate());
         name.setText(test.getUser());
