2f27dac2d936b99c12d2c91711d0ef10dfec7d88
TorinF <tfaes@sweetwaterschools.net>
2019-07-03 15:13:39 -0700

    Friend Request update

--- app/src/main/java/com/Provendor/Provendor/ViewProfile.java
+++ app/src/main/java/com/Provendor/Provendor/ViewProfile.java
@@ -1,36 +1,84 @@
 package com.Provendor.Provendor;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ViewProfile extends AppCompatActivity {
 
     private Button friendBut;
 
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentViewer;
+    private ProfileClass viewedProfile;
+    private FirebaseFirestore db;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
-        final ProfileClass thisprofile = ProfileList.currentUpload;
-        TextView userID = findViewById(R.id.UserID1);
-        userID.setText(thisprofile.getUser());
 
-        //Friend Request button and action
+        mAuth = FirebaseAuth.getInstance();
+        currentViewer = mAuth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
+        viewedProfile = ProfileList.currentUpload;
+
+        TextView userID = findViewById(R.id.UserID1);
+        userID.setText(viewedProfile.getUser());
+
+
+        //Friend Request button and actions
         friendBut = findViewById(R.id.friendBut);
+
+        //Change the button if Friend request is pending
+        {
+            String viewer = currentViewer.getUid();
+            String viewedUser = viewedProfile.getUser();
+            DocumentReference invite = db.collection("userdata").document(viewer).collection("requests").document(viewedUser);
+
+            //if there is friend request, displays pending
+            invite.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        friendBut.setText(R.string.pending);
+                        //Grey background
+                        friendBut.setBackgroundColor(Color.parseColor("#cccccc"));
+                        //Can't send more requests
+                        friendBut.setClickable(false);
+                    }
+                }
+            });
+        }
+
         friendBut.setOnClickListener(
                 new View.OnClickListener() {
                     public void onClick(View v) {
-//                        sender =;
-//                        recipient =;
-//                        Invitation invite = new Invitation(sender, recipient, time);
+                        String sender = currentViewer.getUid();
+                        String recipient = viewedProfile.getUser();
+                        Invitation invite = new Invitation(sender, recipient);
+
+                        //Add invite to sender's and reciever's nodes
+                        db.collection("userdata").document(sender).collection("requests").document(recipient).set(invite);
+                        db.collection("userdata").document(recipient).collection("pending").document(sender).set(invite);
                     }
                 }
         );
-
-
     }
+
 }
